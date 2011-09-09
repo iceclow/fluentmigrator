13ad410b03ed58873de91d055a137ed8bdd07225
iceclow <iceclow@gmail.com>
2011-09-09 04:15:29 +0200

    Spike implementation for ProcessorFactory with MEF
    -- Still Compisition needs to be strongly signed

--- src/FluentMigrator.Runner/Processors/ProcessorFactory.cs
+++ src/FluentMigrator.Runner/Processors/ProcessorFactory.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using FluentMigrator.Runner.Shared.Processors;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 
 namespace FluentMigrator.Runner.Processors
 {
     public static class ProcessorFactory
     {
+        private static CompositionBatch batch;
+        private static CompositionContainer container;
+
+        static ProcessorFactory()
+        {
+            DirectoryCatalog catalog = new DirectoryCatalog("directory");
+
+            container = new CompositionContainer(catalog);
+
+            batch = new CompositionBatch();
+
+            container.Compose(batch);
+        }
+
         public static IMigrationProcessorFactory GetFactory(string processorName)
         {
             foreach (var factory in Factories)
             {
                 var type = factory.GetType();
                 var name = type.Name;
+
                 if (name.StartsWith(processorName, StringComparison.OrdinalIgnoreCase))
                 {
                     return factory;
@@ -25,51 +42,19 @@ namespace FluentMigrator.Runner.Processors
 
         public static string ListAvailableProcessorTypes()
         {
-            IEnumerable<Type> processorTypes = typeof(IMigrationProcessorFactory).Assembly.GetExportedTypes()
-                .Where(t => typeof(IMigrationProcessorFactory).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface);
+            //TODO Do not instantiante every imported type
 
-            string processorList = string.Empty;
-            foreach (Type processorType in processorTypes.OrderBy(x => x.Name))
-            {
-                string name = processorType.Name;
+            var types = container.GetExports<IMigrationProcessorFactory>();
 
-                if (!string.IsNullOrEmpty(processorList))
-                    processorList = processorList + ", ";
-                processorList += name.Substring(0, name.IndexOf("ProcessorFactory")).ToLowerInvariant();
-            }
+            return types.Select(x => x.Value.GetType().Name)
+                        .Select(x => x.Substring(0, x.IndexOf("ProcessorFactory")).ToLowerInvariant())
+                        .Aggregate((x, y) => x + "," + y);
 
-            return processorList;
         }
 
         private static object factoriesLock = new object();
-        private static List<IMigrationProcessorFactory> factories;
-        public static IEnumerable<IMigrationProcessorFactory> Factories
-        {
-            get
-            {
-                if (factories == null)
-                {
-                    lock (factoriesLock)
-                    {
-                        if (factories == null)
-                        {
-                            Assembly assembly = typeof(IMigrationProcessorFactory).Assembly;
-                            var types = assembly.GetExportedTypes().Where(t => typeof(IMigrationProcessorFactory).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract).ToList();
-                            factories = new List<IMigrationProcessorFactory>(types.Count);
-                            foreach (var type in types)
-                            {
-                                var instance = Activator.CreateInstance(type) as IMigrationProcessorFactory;
-                                if (instance != null)
-                                {
-                                    factories.Add(instance);
-                                }
-                            }
-                        }
-                    }
-                }
 
-                return factories;
-            }
-        }
+        [ImportMany(typeof(IMigrationProcessorFactory), AllowRecomposition = true)]
+        public static IEnumerable<IMigrationProcessorFactory> Factories;
     }
 }
